126feb15e102e4d16d09e00f1bdc69969415a24d
nizam19 <officialnizam@gmail.com>
2020-04-07 23:15:53 +0530

    added templates

---
+++ templates/template.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+ 
+#define  fastin    ios::sync_with_stdio(false),cin.tie(0)
+#define  int       long long
+ 
+#define  pb        push_back
+#define  pf        push_front
+#define  rb        pop_back
+#define  rf        pop_front
+#define  xx        first
+#define  yy        second
+#define  all(x)    x.begin(),x.end()
+#define  rall(x)   x.rbegin(),x.rend()
+ 
+#define  endl      '\n'
+ 
+using namespace std;
+ 
+const int MOD  =  998244353;
+const int maxn =  1e6+3;
+
+
+
+void testCase(){
+    
+}
+ 
+signed main(){
+
+    fastin;
+    
+    int t=1;
+    
+    cin>>t;
+ 
+    while(t--){
+        testCase();
+    }
+ 
+    return 0;
+}
---
+++ templates/template.py
@@ -0,0 +1,7 @@
+from sys import stdin
+input = lambda : stdin.readline()
+
+ii = lambda  :  int  ( input() )
+mi = lambda  :  map  ( int, input().split() )
+li = lambda  :  list ( map ( int, input().split() ))
+
